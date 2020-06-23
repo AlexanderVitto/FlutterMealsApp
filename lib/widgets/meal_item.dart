42c4fa5c7aeed8b048f8ecff621c77f9c918b83b
AlexanderVitto <alexandervitto116@gmail.com>
2020-06-23 14:15:11 +0700

    Final

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -50,7 +50,11 @@ class MealItem extends StatelessWidget {
     Navigator.of(context).pushNamed(
       MealDetailScreen.routeName,
       arguments: id,
-    );
+    ).then((result){
+      if(result != null){
+
+      }
+    });
   }
 
   @override
